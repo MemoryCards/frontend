8b1b3c3a53ae0f3b3afc8f1c3885fd7176c6a29a
maurycywoznica <mjw11@wp.pl>
2023-04-05 17:11:14 +0200

    initial config

---
+++ mc-web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from "./components/nav";
+function App() {
+  return (
+    <>
+      <Navigation></Navigation>
+    </>
+  );
+}
+export default App;
---
+++ mc-web/src/components/nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navigation(){
+    return (
+        <>
+        <p>Hello</p>
+        <p>Elo</p>
+        <p>Melo</p>
+        </>
+    )
+}
+export default Navigation
